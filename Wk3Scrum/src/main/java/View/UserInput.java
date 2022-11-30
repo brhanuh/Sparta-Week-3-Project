2a6edde98f6479c494d6e02debfcc9445c599d39
patrykblakala <patrykblakala108@gmail.com>
2022-11-30 22:11:00 +0000

    adjusting method for number range, user entering non numerical values and handling exceptions

--- Wk3Scrum/src/main/java/View/UserInput.java
+++ Wk3Scrum/src/main/java/View/UserInput.java
@@ -1,14 +1,30 @@
 package View;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInput {
 
     public static int getUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a number of employees");
 
-        int numberOfEmployees = scanner.nextInt();
-        return  numberOfEmployees;
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        boolean valid = false;
+        while (!valid) {
+            System.out.print("Enter number between 1 and 1000 ");
+            try {
+                num = scanner.nextInt();
+                if (num > 0 && num <= 1000) {
+                    valid = true;
+                } else {
+                    System.out.println("Out of range.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Not a valid number.");
+                scanner.next();
+            }
+        }
+        System.out.println("This is a list of " + num + " employees");
+        return num;
     }
 }
