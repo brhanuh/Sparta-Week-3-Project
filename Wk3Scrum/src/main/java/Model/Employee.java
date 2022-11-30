ef77eef58835e6f4b5cd6ef321c8a21ea966d152
patrykblakala <patrykblakala108@gmail.com>
2022-11-30 17:25:14 +0000

    Creating Employee class

---
+++ Wk3Scrum/src/main/java/Model/Employee.java
@@ -0,0 +1,23 @@
+package Model;
+
+import java.util.Date;
+
+public class Employee {
+
+    private int employeeNumber;
+    private Date dateOfBirth;
+    private String firstName;
+    private String lastName;
+    private char gender;
+    private Date dateOfEmployment;
+
+    public Employee(int employeeNumber, Date dateOfBirth, String firstName, String lastName, char gender, Date dateOfEmployment) {
+        this.employeeNumber = employeeNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.dateOfEmployment = dateOfEmployment;
+    }
+
+}
