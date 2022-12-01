b6374cdc0cb08396bf5a6b258b43aa43361ef35e
patrykblakala <patrykblakala108@gmail.com>
2022-12-01 15:21:02 +0000

    adding getters and setters and overriding toString()

--- Wk3Scrum/src/main/java/Model/Employee.java
+++ Wk3Scrum/src/main/java/Model/Employee.java
@@ -20,17 +20,63 @@ public class Employee {
         this.dateOfEmployment = dateOfEmployment;
     }
 
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeNumber=" + employeeNumber +
+                ", dateOfBirth=" + dateOfBirth +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                ", dateOfEmployment=" + dateOfEmployment +
+                '}';
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfEmployment() {
+        return dateOfEmployment;
+    }
+
+    public void setDateOfEmployment(Date dateOfEmployment) {
+        this.dateOfEmployment = dateOfEmployment;
+    }
 }
-/*
-Suggestion, make ths class abstract
-    public abstract class Employee {
-
-    public abstract void employeeNumber;
-    public abstract void dateOfBirth;
-    public abstract void firstName;
-    public abstract void lastName;
-    public abstract void gender;
-    public abstract void dateOfEmployment;
-
-    with another abstract class to generate the individual spartans
- */
