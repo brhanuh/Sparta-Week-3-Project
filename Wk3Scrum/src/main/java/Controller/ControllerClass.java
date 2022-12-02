6bf8170738cc74800087cf9b9650ac7397f9135d
patrykblakala <patrykblakala108@gmail.com>
2022-12-02 17:11:17 +0000

    adding a method

--- Wk3Scrum/src/main/java/Controller/ControllerClass.java
+++ Wk3Scrum/src/main/java/Controller/ControllerClass.java
@@ -1,4 +1,26 @@
 package Controller;
 
+import Model.BinaryTree;
+import Model.Employee;
+import Model.EmployeeFactory;
+import Model.EmployeeListConverter;
+import View.UserInput;
+
+import java.util.List;
+
 public class ControllerClass {
+
+    EmployeeListConverter employeeListConverter = new EmployeeListConverter();
+    BinaryTree binaryTree = new BinaryTree();
+
+    public List<Employee>  printEmployeesToConsole() {
+        List<Employee> listOfEmployeeObjects = EmployeeFactory.getListOfEmployeeObjects(UserInput.getUserInput());
+        {
+            for (Employee e:listOfEmployeeObjects ) {
+                System.out.println(e.fullToString());
+            }
+        } return listOfEmployeeObjects;
+    }
+
+
 }
