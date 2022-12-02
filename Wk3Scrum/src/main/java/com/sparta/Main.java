6a44fbf9f134fd7543d07020dfa008e17abdf016
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2022-12-02 13:15:19 +0000

    Got search method working, also created a more verbose toString called "fullToString". Using EmployeeListConverter to make the binary tree now because the requirements ask for it. Changed "==" in search to .equals().

--- Wk3Scrum/src/main/java/com/sparta/Main.java
+++ Wk3Scrum/src/main/java/com/sparta/Main.java
@@ -14,31 +14,27 @@ public class Main {
 
         String[] employeeSample = EmployeeFactory.getEmployees(UserInput.getUserInput());
         ArrayList<String[]> newStrings = new ArrayList<>();
-        List<Employee> EmployeeList = new ArrayList();
-        for(String s : employeeSample){
+        List<Employee> EmployeeList = new ArrayList<>();
+        for (String s : employeeSample) {
             newStrings.add(s.split(","));
         }
 
         //TODO - Should add a check for null here and not add that to the list
-        for(String[] empData : newStrings){ // Turning the sample of employees into unique Employee objects and storing that employee object in a List
+        for (String[] empData : newStrings) { // Turning the sample of employees into unique Employee objects and storing that employee object in a List
 //          At this point you can get each part of employee data using the index values e.g. empData[1]
-            EmployeeList.add(EmployeeFactory.makeEmployee(empData[0],empData[1],empData[2],empData[3],empData[4],empData[5]));
+            EmployeeList.add(EmployeeFactory.makeEmployee(empData[0], empData[1], empData[2], empData[3], empData[4], empData[5]));
         }
 
-        BinaryTree bt = new BinaryTree();
+        BinaryTree bt = (new EmployeeListConverter().convert(EmployeeList));
         final BinaryTree.Node root = new BinaryTree.Node(EmployeeList.get(0));
-        for (Employee e: EmployeeList) {
+        for (Employee e : EmployeeList) {
 //            System.out.println("Current employee " + e.toString());
 //            System.out.println("Root: " + root.toString());
 //            System.out.println("hello");
-            bt.add(root,e);
+            bt.add(root, e);
         }
-        //System.out.println("starting node: " + root.toString());
-        //bt.printNodes(root);
-        bt.search(root, "Bail");
-
-        EmployeeListConverter employeeListConverter = new EmployeeListConverter();
-        System.out.println(employeeListConverter.convert(EmployeeList));
+//        bt.printNodes(root);
+        bt.search(root,EmployeeList.get(1).getLastName());
 
     }
 }
